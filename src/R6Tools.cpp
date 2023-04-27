a5461cfbc05f0e863ae7c2331bde720e299264d5
Machenta <66017341+Machenta@users.noreply.github.com>
2023-04-27 20:05:38 +0100

    Minor Improvements

--- src/R6Tools.cpp
+++ src/R6Tools.cpp
@@ -13,19 +13,7 @@ R6Tools::R6Tools(QWidget *parent)
     onSecondComboBoxIndexChanged(ui.site_combo->currentIndex());
 
     mapList = { "Bank", "Border", "Chalet", "Clubhouse", "Coastline", "Consulate", "Favela", "Fortress", "Hereford Base", "House", "Kafe Dostoyevsky", "Kanal", "Oregon", "Outback", "Presidential Plane", "Skyscraper", "Theme Park", "Tower", "Villa", "Yacht" };
-    BankSites = { "Archives/Tellers", "CEO Office/Staff Room", "Open Area/Kitchen", "Staff Room/CEO Office", "Tellers/Archives" };
-    BorderSites = { "Armory/Archives", "Bathroom/Tellers", "Customs/Supply Room", "Ventilation/Workshop", "Workshop/Ventilation" };
-    ChaletSites = { "Bar/Gaming Room", "Dining Room/Kitchen", "Kitchen/Dining Room", "Library/Living Room", "Living Room/Library" };
-    ClubhouseSites = { "Bar/Stock Room", "CCTV/Cash Room", "Church/Arsenal Room", "Gym/Bedroom", "Master Bedroom/Gym" };
-    CoastlineSites = { "Blue Bar/Sunrise Bar", "Hookah Lounge/Billiards Room", "Kitchen/Service Entrance", "Penthouse/Theater", "Theater/Penthouse" };
-    ConsulateSites = { "Consul Office/Meeting Room", "Garage/Cafeteria", "Lobby/Press Room", "Meeting Room/Consul Office", "Press Room/Lobby" };
-    FavelaSites = { "Aunt's Apartment/Armory Room", "Biker's Apartment/Grow Room", "Football Apartment/Workshop", "Packaging Room/Back Stairs", "Showers/Toilet" };
-    FortressSites = { "Armory/Throne Room", "Bathroom/Bedroom", "Commanders Office/Dormitory", "Great Hall/Dining Room", "Throne Room/Armory" };
-    
-    siteOptions[0] = { "Archives/Tellers", "CEO Office/Staff Room", "Open Area/Kitchen", "Staff Room/CEO Office", "Tellers/Archives" };
-    siteOptions[1] = { "Armory/Archives", "Bathroom/Tellers", "Customs/Supply Room", "Ventilation/Workshop", "Workshop/Ventilation" };
-    siteOptions[2] = { "Bar/Gaming Room", "Dining Room/Kitchen", "Kitchen/Dining Room", "Library/Living Room", "Living Room/Library" };
-    
+
     siteList = { BankSites, BorderSites, ChaletSites, ClubhouseSites, CoastlineSites, ConsulateSites, FavelaSites };
         
     for (const QString& option : mapList) {
@@ -42,15 +30,6 @@ R6Tools::R6Tools(QWidget *parent)
         baseDir.mkpath(mapName);
     }
 
-
-    //QPixmap pixmap("maps/Bank/Villa.png");
-    //ui.banLabel->setPixmap(pixmap);
-    //ui.banLabel->setScaledContents(true);
-    //
-    //QPixmap pixmap2("maps/Bank/Villa.png");
-    //ui.setupLabel->setPixmap(pixmap2);
-    //ui.setupLabel->setScaledContents(true);
-    //loadMap("Villa");
 }
 
 
@@ -88,9 +67,6 @@ void R6Tools::loadMap(const QString& map)
             QPixmap pixmap(fileInfo.filePath());
             images.append(pixmap);
 		}
-
-
-        
 
         // Check if the file name contains '_bans', and if so, set the pixmap to the banLabel
         if (fileName.contains("_bans")) {
